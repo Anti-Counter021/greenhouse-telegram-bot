1d91bbb45ffe386c0e91fe3dbc5a9342f5239907
counter0021 <ara123rus123ara@gmail.com>
2021-07-10 14:52:54 +0300

    Reviews

--- handlers/reviews_handlers.py
+++ handlers/reviews_handlers.py
@@ -14,3 +14,48 @@ async def get_reviews(message: types.Message, state: FSMContext):
 
     response = request_review().reviews()
     await parse_reviews(response, message, state)
+
+
+@dispatcher.message_handler(commands=['new-review'], state=[LoginState, MakeOrderState, ReviewState])
+async def create_review(message: types.Message):
+
+    await message.answer('Создание отзыва')
+
+    await message.answer('Введите оценку, которую вы поставите сайту:')
+
+    dispatcher.register_message_handler(set_appraisal_review, state=ReviewState.appraisal)
+    dispatcher.register_message_handler(set_comment_review, state=ReviewState.comment)
+
+    await ReviewState.appraisal.set()
+
+
+async def set_appraisal_review(message: types.Message, state: FSMContext):
+
+    try:
+        appraisal = int(message.text)
+        if not 0 < appraisal < 6:
+            await message.answer('Оценка должна быть не ниже 1 и не выше 5!')
+            return
+    except ValueError:
+        await message.answer('Введите пожалуйста число!')
+        return
+
+    async with state.proxy() as data:
+        data['appraisal'] = message.text
+
+        await message.answer('Введите комментарий к отзыву')
+
+        await ReviewState.comment.set()
+
+
+async def set_comment_review(message: types.Message, state: FSMContext):
+
+    async with state.proxy() as data:
+        data['comment'] = message.text
+
+        request_review(data['token']).post_json({**data})
+
+        dispatcher.message_handlers.unregister(set_appraisal_review)
+        dispatcher.message_handlers.unregister(set_comment_review)
+
+        await message.answer('Отзыв был создан успешно!')
--- server_requests/reviews.py
+++ server_requests/reviews.py
@@ -3,6 +3,8 @@ from .base_request import AbstractRequest
 
 class ReviewRequest(AbstractRequest):
 
+    url = 'reviews/'
+
     def __init__(self, token=None):
         if token is not None:
             self.headers = {**self.headers, 'Authorization': f'Token {token}'}
